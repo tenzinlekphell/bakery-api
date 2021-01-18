fa318aa6a04a13cde05ab535781541633e673b2c
Tenzin Lekphell <tenzin.lekphell@ryerson.ca>
2021-01-17 22:51:09 -0500

    new locations

--- bakeryAPI.js
+++ bakeryAPI.js
@@ -1,12 +1,25 @@
-//fetch bakery data
-let bakeryAPI = fetch('https://my-bakery-api.herokuapp.com/bakeries')
-    .then(res => {
-        if (res.ok) {
-            console.log('Successful')
-            return res.json();
-        } else {
-            console.log("Not successful")
-        }
-    })
-    .then(data => console.log(data))
-    .catch(error => console.log('Error'))
+//Fetches bakery data by adding functionality to elementId "getUsers"
+document.getElementById("getUsers").addEventListener("click", getUsers);
+
+//Callback function to get bakery data
+function getUsers(e) {
+  e.preventDefault();
+
+  //Fetch bakery data
+
+  fetch("https://my-bakery-api.herokuapp.com/bakeries")
+    .then((res) => res.json())
+    .then((data) => {
+      let output = '<h2 class="mb-4">Bakeries Found</h2>';
+      data.forEach(function (bakery) {
+        output += `
+                            <ul class="list-group mb-3">
+                                <li class="list-group-item">Bakery Name: ${bakery.name}</li>
+                                <li class="list-group-item">Website: ${bakery.website}</li>
+                                <li class="list-group-item">Location: ${bakery.location}</li>
+                            </ul>
+                        `;
+        document.getElementById("output").innerHTML = output;
+      });
+    });
+}
