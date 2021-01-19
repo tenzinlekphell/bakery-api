9b446c44e1ec2221066a73e5848e247cfe42fa7c
Tenzin Lekphell <tenzin.lekphell@ryerson.ca>
2021-01-18 19:49:28 -0500

    revised code

--- bakeryAPI.js
+++ bakeryAPI.js
@@ -1,25 +1,43 @@
-//Fetches bakery data by adding functionality to elementId "getUsers"
-document.getElementById("getUsers").addEventListener("click", getUsers);
+//Select input value element
+//Select container elements (name, website, location)
+const inputValue = document.querySelector("#input-search");
+const nameContainer = document.querySelector('.main__profile-name');
+const websiteContainer = document.querySelector('.main__profile-website');
+const locationContainer = document.querySelector('.main__profile-location');
 
-//Callback function to get bakery data
-function getUsers(e) {
-  e.preventDefault();
+//Fetch bakery data using the async and await method
+//fetchBakeries function accepts a parameter
+const fetchBakeries = async(city) => {
+  const api_call = await fetch(`https://my-bakery-api.herokuapp.com/bakeries/${city}`);
+  const data = await api_call.json();
+  return { data }
+};
 
-  //Fetch bakery data
-
-  fetch("https://my-bakery-api.herokuapp.com/bakeries")
-    .then((res) => res.json())
-    .then((data) => {
-      let output = '<h2 class="mb-4">Bakeries Found</h2>';
-      data.forEach(function (bakery) {
-        output += `
-                            <ul class="list-group mb-3">
-                                <li class="list-group-item">Bakery Name: ${bakery.name}</li>
-                                <li class="list-group-item">Website: ${bakery.website}</li>
-                                <li class="list-group-item">Location: ${bakery.location}</li>
-                            </ul>
-                        `;
-        document.getElementById("output").innerHTML = output;
-      });
-    });
+//showData function uses the fetchBakeries function to fetch data
+//the argument used in fetchBakeries is the inputValue variable
+//inputValue variable is the input field element retrieved by querySelector
+//inputValue.value is the current value in the input field
+//use a promise .then method to get a response back
+//write html inside the containers by using the ES6 string literal method
+const showData = () => {
+  fetchBakeries(inputValue.value).then((res) => {
+    console.log(res);
+    nameContainer.innerHTML = `Name: <span class="main__profile-value">${res.data.name}</span>`;
+    websiteContainer.innerHTML = `Website: <span class="main__profile-value">${res.data.website}</span>`;
+    locationContainer.innerHTML = `Location: <span class="main__profile-value">${res.data.location}</span>`;
+  })
 }
+
+//Select search bakeries button
+//Add a click event to the button and use the showData function 
+const searchBakeriesBtn = document.getElementById("getBakeries")
+searchBakeriesBtn.addEventListener("click",  () => {
+  showData();
+});
+
+
+
+
+
+
+
